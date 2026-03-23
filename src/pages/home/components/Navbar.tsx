322ae9e142751d026d2b6ef1f232761c8f1f9b89
CristianJavierr <cristianjavvierg@gmai.com>
2026-03-23 15:40:43 -0400

    Remove navbar bottom border

--- src/pages/home/components/Navbar.tsx
+++ src/pages/home/components/Navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
   }, []);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-white border-b border-gray-100">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-white">
       <div className="px-5 md:px-8 lg:px-10 h-16 flex items-center">
         <div className="max-w-6xl mx-auto w-full flex items-center justify-between">
 
